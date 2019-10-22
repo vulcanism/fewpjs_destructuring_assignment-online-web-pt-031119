714259624ccbb8a9936c72de5e7bce11a5534ca0
Savannah C <sgcorrell@gmail.com>
2019-10-22 15:28:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ const k_muppet = {
 const [moo, neigh, baa, oink, cluck] = farmAnimals.split(" ")
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them Bessie, Dolly, Babe, and Little.
+const [Bessie, , Dolly, Babe, Little] = farmAnimals.split(" ")
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
 
